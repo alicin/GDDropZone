3b78f2248b347163dad1624bc8f96d2983c96cf2
Ali Can Bardakci <alicanbardakci@gmail.com>
2017-05-27 15:58:14 -0400

    Initial Commit

---
+++ GDDropZone/GDDropZone.h
@@ -0,0 +1,19 @@
+//
+//  GDDropZone.h
+//  GDDropZone
+//
+//  Created by Ali Can Bardakci on 27/05/2017.
+//  Copyright © 2017 Tavsanlar AS. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GDDropZone.
+FOUNDATION_EXPORT double GDDropZoneVersionNumber;
+
+//! Project version string for GDDropZone.
+FOUNDATION_EXPORT const unsigned char GDDropZoneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GDDropZone/PublicHeader.h>
+
+
